4ec5b992dd91f283043e2e179bcb5147e026da9d
John Nguyen <johnhvn94@gmail.com>
2019-01-10 17:34:46 -0800

    Add files via upload

---
+++ Hw1.java
@@ -0,0 +1,15 @@
+public class Hw1 {
+
+   public static void main(String[] args) {
+      int x = 0; // n_0
+      int y = 1; // n_1
+      System.out.print(x);
+      for (int i = 0; i < 12; i++) {
+         System.out.print(", " + y);
+         int temp = x;
+         x = y;
+         y = temp + y;
+      }
+      System.out.println();
+   }
+}
