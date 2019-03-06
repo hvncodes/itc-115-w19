f8b40bc432dd9d0eccb972b9d70b3e4f01111d24
John Nguyen <johnhvn94@gmail.com>
2019-03-06 11:59:35 -0800

    Update ProductMain.java

--- mystery/ProductMain.java
+++ mystery/ProductMain.java
@@ -2,7 +2,7 @@
 public class ProductMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// TODO Auto-generated method stub
 		Fruit apple = new Fruit("Apple", 100000001, 
 					"A round fruit of a tree of the rose family, which typically has thin red or green skin and crisp flesh.",
 				       0.10, 55);
