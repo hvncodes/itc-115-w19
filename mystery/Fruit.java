b93a83fd71e33ffec27d9c6782adf2c400e2d242
John Nguyen <johnhvn94@gmail.com>
2019-03-06 12:50:35 -0800

    Update Fruit.java

--- mystery/Fruit.java
+++ mystery/Fruit.java
@@ -3,17 +3,36 @@ import java.util.Locale;
 
 public class Fruit extends Product {
 	//FIELDS
-	String Color
+	private String color;
+    
+    //CONSTRUCTOR
+    public Fruit(String name, int code, String description, double price, int count, String color) {
+		if (name == null || code == null || description == null || price == null || count == null || color == null) {
+			throw new NullPointerException();
+        }
+		this.name = name;
+		this.code = code;
+		this.description = description;
+		this.price = price;
+		this.count = count;
+        this.color = color;
+	}
 	
 	@Override
 	public String getPriceFormatted() {
-        	String formattedPrice = NumberFormat.getCurrencyInstance().format(this.price);
-        	return formattedPrice;
+        String formattedPrice = NumberFormat.getCurrencyInstance().format(this.price);
+        return formattedPrice;
 	}
 	
 	@Override
 	public void toString() {
-		//spit out data
+		System.out.println(this.color);
+        System.out.println(this.name);
+        System.out.println(this.code);
+        System.out.println(this.description);
+        System.out.println(this.value);
+        System.out.println(this.count);
+        System.out.println(this.color);
 	}
 	
 }
