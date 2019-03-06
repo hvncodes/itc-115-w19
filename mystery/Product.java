c7e54bf8c76669be5ff35200c0e5aeb6b8b14b9d
John Nguyen <johnhvn94@gmail.com>
2019-03-06 11:37:36 -0800

    Update Product.java

--- mystery/Product.java
+++ mystery/Product.java
@@ -5,8 +5,20 @@ public abstract class Product {
 	private String name;
 	private int code;
 	private String description;
-	private double value;
+	private double price;
 	private int count;
+	
+	//CONSTRUCTOR
+	public Product(String name, int code, String description, double price, int count) {
+		if (name == null || code == null || description == null || value == null || count == null) {
+			throw new NullPointerException();
+        	}
+		this.name = name;
+		this.code = code;
+		this.description = description;
+		this.value = price;
+		this.count = count;
+	}
 	
 	//METHODS
 	public abstract String getPriceFormatted();
