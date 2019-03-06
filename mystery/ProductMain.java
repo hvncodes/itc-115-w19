4a7128f8f27f5cc3878220724785e3de4f2b8d0c
John Nguyen <johnhvn94@gmail.com>
2019-03-06 12:12:07 -0800

    Update ProductMain.java

--- mystery/ProductMain.java
+++ mystery/ProductMain.java
@@ -4,8 +4,8 @@ public class ProductMain {
 	public static void main(String[] args) {
 	// TODO Auto-generated method stub
 		Fruit apple = new Fruit("Apple", 100000001, 
-					"A round fruit of a tree of the rose family, which typically has thin red or green skin and crisp flesh.",
-				       0.10, 55);
+                                "A round fruit of a tree of the rose family, which typically has thin red or green skin and crisp flesh.",
+                                0.10, 55);
 		//apple.assestDescription(); prints a small desc
 		//System.out.println(carrot.getMarketValue())
 	}
