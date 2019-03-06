52d0e081400ef37eb212f5056b1e820ea3bc8533
John Nguyen <johnhvn94@gmail.com>
2019-03-06 12:10:31 -0800

    Update Product.java

--- mystery/Product.java
+++ mystery/Product.java
@@ -1,5 +1,5 @@
 //REPRESENTS A GENERIC PRODUCT
-// ##################################GETTERS AND SETTERS################################
+// #############################  GETTERS AND SETTERS  #############################
 public abstract class Product {
 	//FIELDS
 	private String name;
@@ -7,18 +7,6 @@ public abstract class Product {
 	private String description;
 	private double price;
 	private int count;
-	
-	//CONSTRUCTOR
-	public Product(String name, int code, String description, double price, int count) {
-		if (name == null || code == null || description == null || value == null || count == null) {
-			throw new NullPointerException();
-        	}
-		this.name = name;
-		this.code = code;
-		this.description = description;
-		this.value = price;
-		this.count = count;
-	}
 	
 	//METHODS
 	public abstract String getPriceFormatted();
