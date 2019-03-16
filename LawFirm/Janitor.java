c31ee6705519a9e1a25e647324dd6e6d93dde12e
John Nguyen <johnhvn94@gmail.com>
2019-03-15 23:48:48 -0700

    Create Janitor.java

---
+++ LawFirm/Janitor.java
@@ -0,0 +1,21 @@
+
+public class Janitor extends Employee {
+    public int showHours() {
+        //2x work week
+        return 2 * super.showHours();
+    }
+    
+    public double showSalary() {
+        //10000 less than avg employee
+        return super.showSalary() - 10000;
+    }
+    
+    public int showVacation() {
+        //half vacation days
+        return super.showVacation() / 2;
+    }
+    
+    public void clean() {
+        //"Workin' for the man."
+    }
+}
